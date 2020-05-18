62f2e1c286b91de2579269af90d5fc011ab4dd8b
taimin-greatest <61199420+taimin-greatest@users.noreply.github.com>
2020-05-18 16:15:10 +0900

    파일출력

--- Project1/Project1/file.cpp
+++ Project1/Project1/file.cpp
@@ -11,12 +11,10 @@ void File_Write(Student* list)
 		exit(-1);
 	}
 	
-	fputs("\t  |\t\t   |\t\t    |\n   ID\t  |\t Teamname  |   \t    Company |", fp);
-	fputs("\n__________|________________|________________|\n\t  |\t\t   |\t\t    |\n", fp);
 
 	for (int i = 0; i < 6; i++)
 	{
-		fprintf(fp, "%5d\t  |\t%10s |%15s |\n",
+		fprintf(fp, "%5d\t %10s %15s\n",
 			(list + i)->id, (list + i)->teamname, (list + i)->company);
 	}
 
